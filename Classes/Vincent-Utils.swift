6292813ba24cebbf333598b5be029f23ac3508b1
Kai Straßmann <derkai@gmail.com>
2015-09-29 17:08:21 +0200

    prepared spinner

--- Classes/Vincent-Utils.swift
+++ Classes/Vincent-Utils.swift
@@ -70,17 +70,25 @@ public extension UIImageView {
         
         guard let url = url else {return}
         
-        
+        if let vincentImageView = self as? VincentImageView where vincentImageView.showsSpinner {
+            vincentImageView.activityIndicator.startAnimating()
+        }
         self.downloadTaskIdentifier = Vincent.sharedInstance.downloadImageFromUrl(url, cacheType: cacheType, success: {[weak self] image in
             self?.downloadTaskIdentifier = nil
             dispatch_async(dispatch_get_main_queue(), {
                 self?.image = image
+                if let vincentImageView = self as? VincentImageView where vincentImageView.showsSpinner {
+                    vincentImageView.activityIndicator.stopAnimating()
+                }
                 completion?(error: nil, image: image)
             })
         }, error: {[weak self] error in
             if error.code != -999 {
                 self?.downloadTaskIdentifier = nil
                 dispatch_async(dispatch_get_main_queue(), {
+                    if let vincentImageView = self as? VincentImageView where vincentImageView.showsSpinner {
+                        vincentImageView.activityIndicator.stopAnimating()
+                    }
                     completion?(error: error, image: nil)
                 })
             }
@@ -187,13 +195,74 @@ public extension UIButton {
     }
 }
 
-public class FadeInImageView: UIImageView {
+public class VincentImageView: UIImageView {
+    @IBInspectable public var showsSpinner: Bool = true
+    @IBInspectable public var spinnerSize: CGFloat = 50.0 {
+        didSet {
+            spinnerHeightConstraint?.constant = spinnerSize
+            spinnerWidthConstraint?.constant = spinnerSize
+        }
+    }
+    @IBInspectable public var spinnerTintColor: UIColor {
+        set {
+            activityIndicator.tintColor = newValue
+        }
+        get {
+            return activityIndicator.tintColor
+        }
+    }
+    @IBInspectable public var spinnerBackgroundColor: UIColor {
+        set {
+            activityIndicator.backgroundColor = newValue
+        }
+        get {
+            return activityIndicator.backgroundColor ?? UIColor.clearColor()
+        }
+    }
+    
+    @IBInspectable public var spinnerLineWidth: CGFloat = 1.0 {
+        didSet {
+            activityIndicator.lineWidth = spinnerLineWidth
+        }
+    }
+    
+    public private(set) var activityIndicator = ActivityIndicatorView()
+    private var spinnerHeightConstraint: NSLayoutConstraint? = nil
+    private var spinnerWidthConstraint: NSLayoutConstraint? = nil
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    public init() {
+        super.init(frame: CGRect.zero)
+        setup()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    private func setup() {
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(activityIndicator)
+        
+        spinnerWidthConstraint = NSLayoutConstraint(item: activityIndicator, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: spinnerSize)
+        spinnerHeightConstraint = NSLayoutConstraint(item: activityIndicator, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: spinnerSize)
+        addConstraints([spinnerWidthConstraint!, spinnerHeightConstraint!])
+        addConstraint(NSLayoutConstraint(item: activityIndicator, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: activityIndicator, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0))
+    }
+}
+
+public class FadeInImageView: VincentImageView {
     private var fadeInDone: Bool = false
     private var fadeInInProgress: Bool = false
-    @IBInspectable var fadeInDuration: Double = 0.2
+    @IBInspectable public var fadeInDuration: Double = 0.2
     
     override public var image: UIImage? {
-    
         willSet {
             if !fadeInDone && newValue != nil {
                 fadeInDone = true
@@ -207,5 +276,77 @@ public class FadeInImageView: UIImageView {
                 layer.addAnimation(animation, forKey: "fadeIn")
             }
         }
+    }
+}
+
+public class ActivityIndicatorView: UIView {
+    @IBInspectable public var lineWidth: CGFloat = 1.0 {
+        didSet {
+            shapeLayer?.lineWidth = lineWidth
+        }
+    }
+    public private(set) var isAnimating = false
+    private var shapeLayer: CAShapeLayer?
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    public init() {
+        super.init(frame: CGRect.zero)
+        setup()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    private func setup() {
+        hidden = true
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.borderWidth = 0
+        shapeLayer.fillColor = UIColor.clearColor().CGColor
+        shapeLayer.strokeColor = tintColor.CGColor
+        shapeLayer.lineWidth = lineWidth
+        layer.addSublayer(shapeLayer)
+        self.shapeLayer = shapeLayer
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        shapeLayer?.frame = bounds
+        shapeLayer?.path = self.layoutPath().CGPath
+    }
+    
+    public override func tintColorDidChange() {
+        super.tintColorDidChange()
+        shapeLayer?.strokeColor = tintColor.CGColor
+    }
+    
+    private func layoutPath() -> UIBezierPath {
+        let twoPi = M_PI * 2.0
+        let startAngle = CGFloat(0.75 * twoPi)
+        let endAngle = CGFloat(startAngle + CGFloat(twoPi * 0.9))
+        let width = bounds.width
+        return UIBezierPath(arcCenter: CGPoint(x: width / 2.0, y: width / 2.0), radius: width / 2.2, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+    }
+    
+    public func startAnimating() {
+        isAnimating = true
+        let animation = CABasicAnimation(keyPath: "transform.rotation")
+        animation.toValue = 2 * M_PI
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.duration = 1.0
+        animation.repeatCount = Float.infinity
+        shapeLayer?.addAnimation(animation, forKey: "spinner")
+        hidden = false
+    }
+    
+    public func stopAnimating() {
+        isAnimating = false
+        shapeLayer?.removeAnimationForKey("spinner")
+        hidden = true
     }
 }
