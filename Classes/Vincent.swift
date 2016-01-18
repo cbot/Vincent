2972f8539871ff7612930e9f740a82adc5e0f0ab
Kai Straßmann <derkai@gmail.com>
2016-01-18 10:49:40 +0100

    changed default accept header

--- Classes/Vincent.swift
+++ Classes/Vincent.swift
@@ -134,7 +134,7 @@ public typealias RequestModificationClosure = (request: Request) -> Void
         } else {
             let request = downloader.request(url, cachePolicy: cacheType == .ForceDownload ? .ReloadIgnoringLocalCacheData : .UseProtocolCachePolicy, timeoutInterval: timeoutInterval)
             
-            request.header("Accept", withValue: "image/*")
+            request.header("Accept", withValue: "image/jpeg, image/png, image/gif")
             for (key, value) in globalRequestHeaders {
                 request.header(key, withValue: value)
             }
